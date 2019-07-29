059350d13304caab6020a98a9c0407c91fcf81c7
Matija <matijaradofficial@gmail.com>
2019-07-29 08:52:00 +0200

    commit1

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Init hover effect*/
+new hoverEffect({
+  /*where distortion going to happen*/
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: '../img/img1.png',
+  image2: '../img/img2.png',
+  displacementImage: '../img/heightMap.png'
+});
---
+++ app2.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: '../img/img1.png',
+  image2: '../img/img2.png',
+  displacementImage: '../img/strip.png'
+});
